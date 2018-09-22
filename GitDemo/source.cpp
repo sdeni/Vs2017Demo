5ae65fa197b61884970511bdaa2b2eb15d61acd1
SD <denisov.univ@gmail.com>
2018-09-22 18:18:48 +0300

    Added flying star

--- GitDemo/source.cpp
+++ GitDemo/source.cpp
@@ -1,6 +1,26 @@
-#include <iostream>
+#include "conslib.h"
 
 void main()
 {
-	std::cout << "Hi All!!";
+	int xr = 25;
+	int i = 1;
+
+	int k = 1;
+	int x = 1;
+
+	while (true)
+	{
+		setCursorVisibility(false);
+		if (x > 25 || x < 1) {
+			k = -k;
+		}
+		printat(x, 15, "*");
+		pause(50);
+		printat(x, 15, " ");
+
+		x += k;
+	}
+
+
+	waitAnyKey();
 }
